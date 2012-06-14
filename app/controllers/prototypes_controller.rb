398ac6da2e19f2020042558db5ad756d38ff020c
Siri Hedengran <siricat@gmail.com>
2012-06-13 18:16:45 -0700

    set up prototypes structure

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -3,4 +3,19 @@ class PrototypesController < ApplicationController
   def index
   end
   
+  def new
+  end
+  
+  def edit
+  end
+  
+  def show
+  end
+  
+  def log_in
+  end
+  
+  def register
+  end
+  
 end
---
+++ app/helpers/path_helper.rb
@@ -0,0 +1,5 @@
+module PathHelper
+  def void_path
+    "javascript:void(0)"
+  end
+end
