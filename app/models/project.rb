21353703a2de1c9faac8a11a1689ea892e3b4d29
Siri Hedengran <siricat@gmail.com>
2012-07-24 12:07:41 -0700

    create project_user model, add associations to models

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,6 +1,8 @@
 class Project < ActiveRecord::Base
   # attr_accessible :title, :body
   
-  has_many :users
+  has_many :project_users
+  has_many :users through => :project_users
+  
   
 end
---
+++ app/models/project_user.rb
@@ -0,0 +1,7 @@
+class ProjectUser < ActiveRecord::Base
+  # attr_accessible :title, :body
+  
+  belongs_to :project
+  belongs_to :user
+  
+end
