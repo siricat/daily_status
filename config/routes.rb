a31acfcb57578e639d26c674ff67a33ce6d97296
Siri Hedengran <siricat@gmail.com>
2012-07-24 12:55:42 -0700

    change path names for devise in routes, and add links to log in and register on application layout, devise is working

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 DailyStatus::Application.routes.draw do
-  devise_for :users
+  devise_for :users, :path => '/', :path_names => {
+    :sign_in => 'login',
+    :sign_out => 'logout',
+    :sign_up => 'register'
+  }
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
