4d3b08d526799865ccbfbef8807613d627a18325
Siri Hedengran <siricat@gmail.com>
2012-07-25 14:00:05 -0700

    project form is working, can now create, edit and delete projects, little bit of styling

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,9 @@
 class Project < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :name
   
   has_many :project_users
   has_many :users, :through => :project_users
+  
+  validates_presence_of :name
    
 end
