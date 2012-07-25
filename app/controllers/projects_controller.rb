26a3213f5608fcdbcc89a2ad87c7ec69f6f41897
Siri Hedengran <siricat@gmail.com>
2012-07-25 12:38:01 -0700

    ran simple form generator to work with twitter bootstrap, add action details to projects controller

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -17,15 +17,36 @@ class ProjectsController < ApplicationController
   end
   
   def create
+    @project = Project.new(params[:project])
+    if @project.save
+      flash[:notice] = "Project has been created."
+      redirect_to @project
+    else
+      flash[:error] = "Project has not been created."
+      render :action => "new"
+    end
   end
   
   def edit
+    @project = Project.find(params[:id])
   end
   
   def update
+    @project = Project.find(params[:id])
+    if @project.update_attributes(params[:project])
+      flash[:notice] = "Project has been updated."
+      redirect_to @project
+    else
+      flash[:error] = "Project has not been updated."
+      render :action => "edit"
+    end
   end
   
   def destroy
+    @project = Project.find(params[:id])
+    @project.destroy
+    flash[:notice] = "Project has been deleted."
+    redirect_to projects_path
   end
   
 end
